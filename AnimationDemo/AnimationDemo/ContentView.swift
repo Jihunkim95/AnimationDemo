89d0aa1dabb9e2e3d305037fb57ec6fcf8d54799
Jihunkim95 <darak551@naver.com>
2023-12-20 15:10:03 +0900

    add: Multiple Keyframe Tracks 다중 키프레임 트랙
    - 키프레임은 각각 고유한 타이밍을 갖는 별도의 트랙을 정의하여 여러 효과를 독립적으로 애니메이션화할 수 있는 기능을 제공
    - 여러 트랙을 통합함으로써 다양한 속성에 동시에 애니메이션을 적용할 수 있어 더욱 발전된 애니메이션이 가능
    
    - 수직 늘이기, 평행 이동 및 불투명도 속성에 대해 별도의 키프레임 트랙을 정의

--- AnimationDemo/AnimationDemo/ContentView.swift
+++ AnimationDemo/AnimationDemo/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
 //                TransitionView(isButtonVisible: $isButtonVisible)
                 SimplePhaseAnimatorView()
                 PhaseAnimatorView()
+//                KeyframeAnimatorView()
             }
         }
 
